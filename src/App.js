9c214cdd63807dbbcf487a12ebdd90af1ac74252
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-02-23 17:32:40 +0000

    finalizando
    rotas criadas com sucesso, e aplicado estilização de componente por styled-components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import AppRoutes from './pages/routes'
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <AppRoutes />
     </div>
   );
 }
+const GlobalStyle =  createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
 
+  }`
 export default App;
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 async function getPost(id) {
     const response = await fetch(`http://localhost:3000/json/post-${id}.json`)
@@ -22,16 +23,24 @@ const PostDetails = () => {
     }, [])
 
     return (
-        <section>
+        <Section>
             <Link to="/">Voltar</Link>
             <div>
-                <img src={post.image} alt="post.title" />
+                <Image src={post.image} alt="post.title" />
                 <h2>{post.title}</h2>
                 <p>{post.text}</p>
             </div>
 
-        </section>
+        </Section>
     )
 }
+const Section = styled.section`
+padding: 50px;`
 
+const Image = styled.img` 
+width: 100%;`
+   
+
+
+ 
 export { PostDetails }
